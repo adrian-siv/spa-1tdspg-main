edc64f6e36f1f0e8ca0b4a21a66775c4c5bb315d
adrian-siv <adrian.ssiv@gmail.com>
2023-10-04 08:59:49 -0300

    Refatoração do projeto + Add imagens estáticas

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,16 @@
 export default function Home(){
 
+    document.title = "Home";
+    
     return(
         <>
             <div>
                 <h1>HOME</h1>
+                <figure>
+                    <figcaption>
+                        <img src="/online-shopping-5700377_640.png" alt="Banner de Loja Online" />
+                    </figcaption>
+                </figure>
             </div>
         </>
     )
